78713f039b739a235f31eeaaa104e75fb514b8ec
hunterstar323 <jose.montero01@unicatolica.edu.co>
2025-08-20 19:15:26 -0500

    primera actividad realizada

---
+++ src/main/kotlin/edu/unicatolica/kotlin/myself.kt
@@ -0,0 +1,23 @@
+package edu.unicatolica.kotlin
+
+fun main(){
+
+    val name = "Jose David Montero"
+    val id = "408258"
+    val age = 19
+    val semester = "5"
+    val game1 = "Apex Legends"
+    val game2 = "Pokemon"
+    val deltarune = "Deltarune"
+    val serie = "Stranger things"
+    println("HI HI HIII")
+    println("Bueno en español, soy $name (ID: $id), tengo $age años y estudio ingenieria de sistemas en la unicatolica!")
+    println("Actualmente, estoy en quinto ($semester) semestre, estaba viendo las materias en la mañana pero me pase a la noche por trabajo")
+    println("Trabajo en una empresa de desarrollo, he aprendido mucho alli :D")
+    println("Mis gustos se resumen en jugar videojuegos, juego cosas... no tan variadas.")
+    println("Por ejemplo juego $game1 y tambien $game2 ... pero mi juego favorito es $deltarune, me encanta mucho")
+    println("Tambien tengo series favoritas... tampoco muchas, no veo muchas series")
+    println("La serie que mas recuerdo y considero mi favorita es $serie")
+    println("Y eso es todo, esta clase se ve interesante y el profe tambien :D")
+
+}
